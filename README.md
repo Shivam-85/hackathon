df899b63f6ecd403c33fe68938830faa374f677c
Shivam-85 <103090492+Shivam-85@users.noreply.github.com>
2022-09-15 12:39:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hackathon_2022_team13
+Draup Hackathon - End to end Load testing service for APIs
---
+++ load_test/base/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home_view(request,*args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
